506fa9fcfb238af76bf4c01c0beea53a291c8622
stefan_karlberg <stefan.karlberg@smackit.se>
2019-10-13 21:19:47 +0200

    Updates

---
+++ precache-manifest.7216e53b8e041cc204e59f04c4ee3349.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e092016990f098969c8f1257e3d0fca0",
+    "url": "/zooma/index.html"
+  },
+  {
+    "revision": "702e1ddc4da3fe9e00d3",
+    "url": "/zooma/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "c5d48d21537e5ca96fd2",
+    "url": "/zooma/static/css/main.69ccc2d3.chunk.css"
+  },
+  {
+    "revision": "702e1ddc4da3fe9e00d3",
+    "url": "/zooma/static/js/2.35fc8611.chunk.js"
+  },
+  {
+    "revision": "c5d48d21537e5ca96fd2",
+    "url": "/zooma/static/js/main.345224f5.chunk.js"
+  },
+  {
+    "revision": "9bc3f8fb1d85b50cd5d8",
+    "url": "/zooma/static/js/runtime-main.e7abe828.js"
+  },
+  {
+    "revision": "383ad926d2f6f914821fd948d55fc24d",
+    "url": "/zooma/static/media/Serious-womanBW.383ad926.jpg"
+  },
+  {
+    "revision": "dab89531c380dd5044da3c324b54facc",
+    "url": "/zooma/static/media/arrow.dab89531.svg"
+  },
+  {
+    "revision": "6fa566ba0e72dd4dccdc7e49ba38c594",
+    "url": "/zooma/static/media/bg_image.6fa566ba.png"
+  },
+  {
+    "revision": "ce45736d615793996a5a76ca1ed29b45",
+    "url": "/zooma/static/media/blog.ce45736d.png"
+  },
+  {
+    "revision": "2263b97ad844e7a0b8a6b922f0c84387",
+    "url": "/zooma/static/media/fill.2263b97a.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/zooma/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "efd3289fcb8de6c737b2e45e0f2c7817",
+    "url": "/zooma/static/media/trekanten.efd3289f.svg"
+  }
+]);
